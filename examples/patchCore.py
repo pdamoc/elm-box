d66c2a62ab7eb0a8677ef5750b08face86223efa
Peter Damoc <pdamoc@gmail.com>
2017-01-20 14:51:42 +0200

    added a small script that patches the current core to enable the code

---
+++ examples/patchCore.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+
+plat = "./elm-stuff/packages/elm-lang/core/5.0.0/src/Native/Platform.js"
+vdom = "./elm-stuff/packages/elm-lang/virtual-dom/2.0.3/src/Native/VirtualDom.js"
+
+platCont = ""
+with open(plat, "r") as f:
+    platCont = f.read()
+
+
+vdomCont = ""
+with open(vdom, "r") as f:
+    vdomCont = f.read()
+
+if not "spawnLoop:spawnLoop" in platCont:
+    platCont = platCont.replace("initialize: initialize,", "initialize: initialize,\n\tspawnLoop:spawnLoop,\n\tdispatchEffects:dispatchEffects,\n\tsetupEffects:setupEffects,")
+    with open(plat, "w") as f:
+        f.write(platCont)
+
+if not "normalRenderer:normalRenderer" in vdomCont:
+    vdomCont = vdomCont.replace("keyedNode: F3(keyedNode),", "keyedNode: F3(keyedNode),\n\tnormalRenderer:normalRenderer,")
+    with open(vdom, "w") as f:
+        f.write(vdomCont)
+         
